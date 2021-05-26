0642f543f532b179ed2f529d75a23e27ea34b99c
Kozyreva Ann <anyta_winterr@mail.ru>
2021-05-26 15:32:12 +0700

    Add new files

---
+++ Lab3/Models/QuizAnswerModel.cs
@@ -0,0 +1,8 @@
+﻿using Lab3.Utils;
+
+namespace Lab3.Models {
+    public class QuizAnswerModel {
+        [LessThanOrNull(101, ErrorMessage = "Здесь не может быть такого большого числа")]
+        public int? Answer { get; set; }
+    }
+}
---
+++ Lab3/Models/QuizResultModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Models {
+    public class QuizResultModel {
+        public List<QuizQuestionModel> Questions { get; set; }
+
+        public int CorrectAnswerCount {
+            get {
+                return Questions.Count(q => q.AnswerIsCorrect);
+            }
+        }
+
+        public int Count => Questions.Count;
+    }
+}
